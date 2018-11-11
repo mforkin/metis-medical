5158ee71b3806eaf4777c1ddeda5253bc98baad0
Mike Forkin <mforkin@ccri.com>
2018-11-10 22:06:17 -0500

    Specialty Services

--- metis-medical-services/src/main/scala/com/greenleaf/services/SpecialtyService.scala
+++ metis-medical-services/src/main/scala/com/greenleaf/services/SpecialtyService.scala
@@ -4,10 +4,36 @@ import com.greenleaf.database.ConnectionManager
 import com.greenleaf.metis.medical.jooq.generated.Tables._
 import collection.JavaConverters._
 
+case class Specialty (id: Int, name: String)
+case class CandidateSpecialty (name: String)
+
 object SpecialtyService {
   lazy val db = ConnectionManager.db
 
   def getSpecialities = {
-    db.selectFrom(SPECIALTY).fetch.asScala.map(r => (r.getId, r.getName)).toMap
+    db.selectFrom(SPECIALTY)
+      .fetch
+      .asScala
+      .map(r => (r.getId, r.getName)).toMap
+  }
+
+  def getSpecialty (id: Int) = {
+    val speciality = db.selectFrom(SPECIALTY)
+      .where(SPECIALTY.ID.equal(id))
+      .fetchOne
+    Specialty(speciality.getId, speciality.getName)
+  }
+
+  def create (specialty: CandidateSpecialty): Unit = {
+    db.insertInto(SPECIALTY, SPECIALTY.NAME)
+      .values(specialty.name)
+      .execute()
+  }
+
+  def edit (specialty: Specialty) = {
+    db.update(SPECIALTY)
+      .set(SPECIALTY.NAME, specialty.name)
+      .where(SPECIALTY.ID.equal(specialty.id))
+      .execute()
   }
 }
--- metis-medical-services/src/main/scala/com/greenleaf/services/api/SpecialityAPI.scala
+++ metis-medical-services/src/main/scala/com/greenleaf/services/api/SpecialityAPI.scala
@@ -1,11 +1,25 @@
 package com.greenleaf.services.api
 
-import com.greenleaf.services.SpecialtyService
+import com.greenleaf.services.{CandidateSpecialty, Specialty, SpecialtyService}
 
 class SpecialityAPI extends API {
   override val root = "api/specialty"
 
   get("/") {
     SpecialtyService.getSpecialities
+  }
+
+  post("/") {
+    val data = parsedBody.extract[CandidateSpecialty]
+    SpecialtyService.create(data)
+  }
+
+  put("/:id") {
+    val data = parsedBody.extract[Specialty]
+    SpecialtyService.edit(data)
+  }
+
+  get("/:id") {
+    SpecialtyService.getSpecialty(params.as[Int]("id"))
   }
 }
