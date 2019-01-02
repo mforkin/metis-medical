59b828cdc01a06c26216438e209647b23d974141
Mike Forkin <mforkin@ccri.com>
2019-01-01 22:32:41 -0500

    Seeding with new admin

--- metis-medical-database/src/main/sql/seedData.sql
+++ metis-medical-database/src/main/sql/seedData.sql
@@ -3,6 +3,7 @@ INSERT INTO public.specialty(name) values ('Specialty One');
 INSERT INTO USERS (username, password, enabled, specialty_id) VALUES ('mike.forkin@gmail.com', '$2a$10$rQ.bdTyQSNVkTM5N9IXu7uZ1v9Oe5rpDJ.qDQuZS1tOUBq5X9fu6y', true, 1);
 INSERT INTO USERS (username, password, enabled, specialty_id) VALUES ('user', '$2a$10$rQ.bdTyQSNVkTM5N9IXu7uZ1v9Oe5rpDJ.qDQuZS1tOUBq5X9fu6y', true, 1);
 INSERT INTO USERS (username, password, enabled, specialty_id) VALUES ('lauren.k.dunn@gmail.com', '$2a$10$rQ.bdTyQSNVkTM5N9IXu7uZ1v9Oe5rpDJ.qDQuZS1tOUBq5X9fu6y', true, 1);
+INSERT INTO USERS (username, password, enabled, specialty_id) VALUES ('lak3r@virginia.edu', '$2a$10$rQ.bdTyQSNVkTM5N9IXu7uZ1v9Oe5rpDJ.qDQuZS1tOUBq5X9fu6y', true, 1);
 
 
 INSERT INTO AUTHORITIES (username, authority) values ('mike.forkin@gmail.com', 'ROLE_USER');
@@ -10,5 +11,8 @@ INSERT INTO AUTHORITIES (username, authority) values ('mike.forkin@gmail.com', '
 
 INSERT INTO AUTHORITIES (username, authority) values ('lauren.k.dunn@gmail.com', 'ROLE_USER');
 INSERT INTO AUTHORITIES (username, authority) values ('lauren.k.dunn@gmail.com', 'ROLE_ADMIN');
+
+INSERT INTO AUTHORITIES (username, authority) values ('lak3r@virginia.edu', 'ROLE_USER');
+INSERT INTO AUTHORITIES (username, authority) values ('lak3r@virginia.edu', 'ROLE_ADMIN');
 
 INSERT INTO AUTHORITIES (username, authority) values ('user', 'ROLE_USER');
